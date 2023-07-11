8920f1e465053a71f224bb94dbcdaaf8c31520e9
HenriqueCorso <125920727+HenriqueCorso@users.noreply.github.com>
2023-07-11 16:57:09 -0300

    Update pellet.js

--- classes/pellet.js
+++ classes/pellet.js
@@ -1,18 +1,17 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
-
 export class Pellet {
   constructor({ position }) {
+    this.canvas = document.querySelector('canvas');
+    this.ctx = this.canvas.getContext('2d');
     this.position = position
     this.radius = 3
   }
 
   draw() {
-    c.beginPath()
-    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
-    c.fillStyle = 'white'
-    c.fill()
-    c.closePath()
+    this.ctx.beginPath()
+    this.ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+    this.ctx.fillStyle = 'white'
+    this.ctx.fill()
+    this.ctx.closePath()
 
   }
 }
